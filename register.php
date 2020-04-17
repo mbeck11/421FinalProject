6bf61058e12e352e62b6532bc8ee5c38c92c4740
Jacob Malloy <sjakem@att.net>
2020-04-17 02:16:19 -0500

    work on the sign in sign out

--- register.php
+++ register.php
@@ -32,11 +32,11 @@
             $stmt->close();
         }
         if(!empty($username)){
-            $sql = "INSERT INTO Customer (Username, Password, Address, Name, State, Zip) VALUES (?, ?, ?, ?, ?, ?)";
+            $sql = "INSERT INTO Customer (Username, Password, Address, Name, State, Zip, CardNumber) VALUES (?, ?, ?, ?, ?, ?, ?)";
          
             if($stmt = $conn->prepare($sql)){
                 // Bind variables to the prepared statement as parameters
-                $stmt->bind_param("sssssi", $param_username, $param_password, $param_address,$param_name, $param_state,$param_zip);
+                $stmt->bind_param("sssssii", $param_username, $param_password, $param_address,$param_name, $param_state,$param_zip,$param_card);
                 
                 // Set parameters
                 $param_username = $username;
@@ -45,6 +45,7 @@
                 $param_name=$_POST["name"];
                 $param_state=$_POST["state"];
                 $param_zip=intval($_POST["zip"]);
+                $param_card=intval($_POST["cardnumber"]);
                 // Attempt to execute the prepared statement
                 if($stmt->execute()){
                     // Redirect to login page
