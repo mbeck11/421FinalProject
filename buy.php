4955dbcbe2dcaa49ee24f1518de5195661987beb
Jacob Malloy <sjakem@att.net>
2020-04-20 12:00:26 -0500

    get buy to work

--- buy.php
+++ buy.php
@@ -7,26 +7,34 @@
         require "funcs.php";
         $sql = "SELECT Max(Order_Num) as maxOrder from mbeck11_421FinalProject.Orders";
         $result = $conn->query($sql);
-        $r = queryToJson($result);
+        $r = json_decode(queryToJson($result));
         $result->close();
-        print_r($r[0]);
-        $orderNum = ($r[0]->orderNum==null?1:$r[0]->orderNum+1);
-        $sql = "INSERT INTO Orders (SKU,Order_Num,Customer_ID) VALUES (?,?,?)";   
+        $orderNum = ($r[0]->maxOrder==null?1:intval($r[0]->maxOrder)+1);
+        $sql = "INSERT INTO Orders (SKU,Order_Num,Customer_ID,QTY) VALUES (?,?,?,?)";
+        $count = 0;   
         if($stmt = $conn->prepare($sql)){
             // Bind variables to the prepared statement as parameters
-            $stmt->bind_param("sii",$param_SKU,$param_Order_Num,$param_Customer);
+            $stmt->bind_param("siii",$param_SKU,$param_Order_Num,$param_Customer,$param_qty);
             foreach($cart as $i){
                 if($i->number>0){
                     $param_SKU = $i->sku;
                     $param_Order_Num=intval($orderNum);
                     $param_Customer = intval($user->Customer_ID);
+                    $param_qty= intval($user->number);
                     // Attempt to execute the prepared statement
                     if($stmt->execute()){
-                        // Redirect to login page
-                        //header("Location:login.html");
+                        $count++;
+                        if($count==count($cart,COUNT_NORMAL))
+                        {
+                            setcookie("cart","",time()-36000);
+                            header("Location:index.html");
+                        }
                     } else{
                         echo $stmt->error;
                     }
+                }
+                else{
+                    $count++;
                 }
             }
 
